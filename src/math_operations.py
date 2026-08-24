0a059c2813a057c689148ba2cd3edbbd5ba43091
vishalpatil91 <saivishalpp30999@gmail.com>
2026-08-24 23:49:58 +0530

    multiplication added

--- src/math_operations.py
+++ src/math_operations.py
@@ -3,3 +3,7 @@ def add(a,b):
 
 def sub(a,b):
     return a-b 
+
+def mul(a,b):
+    return a*b
+
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,4 @@
-from src.math_operations import add,sub
+from src.math_operations import add,sub, mul
 
 def test_add():
     assert add(2,3)==5
@@ -9,3 +9,8 @@ def test_sub():
     assert sub(4,3)==1
     assert sub(3,3)==0
     assert sub(2,3)==-1
+
+def test_mul():
+    assert mul(2,3)==6
+    assert mul(-8,1)==-8
+
